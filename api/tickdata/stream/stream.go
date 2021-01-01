444b1a99a99b6188a4e96f2d068147bba02c1e6a
Edward Yakop <edward.yakop@gmail.com>
2021-01-01 11:21:48 +0800

    * Bug: Fix bug where log is initialized for every NewStream.

--- api/tickdata/stream/stream.go
+++ api/tickdata/stream/stream.go
@@ -65,6 +65,7 @@ var isLogSetup = false
 // time are in UTC
 func NewStream(symbol string, start time.Time, end time.Time, downloadFolderPath string) *Stream {
 	if !isLogSetup {
+		isLogSetup = true
 		clog.NewConsole(0, clog.ConsoleConfig{
 			Level: clog.LevelInfo,
 		})
